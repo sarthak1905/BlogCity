49c4deb5d27127d78b6051e763ca7556b77d9e0d
sarthak1905 <61883822+sarthak1905@users.noreply.github.com>
2020-09-02 15:09:32 +0530

    Added index

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='blog-home'),
+    path('',views.index, name='blof-index')
+    path('home/', views.PostListView.as_view(), name='blog-home'),
     path('user/<str:username>', views.UserPostListView.as_view(template_name='blog/user_post.html'), name='user-posts'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
--- blog/views.py
+++ blog/views.py
@@ -69,3 +69,5 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin ,DeleteView):
 def about(request):
     return render(request,'blog/about.html',{'title':'About'}) 
 
+def index(request):
+    return render(request,'blog/index.html')
