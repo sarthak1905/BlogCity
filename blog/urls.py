8cd2f750ca92930613445f97b079456c31429bed
sarthak1905 <61883822+sarthak1905@users.noreply.github.com>
2020-09-02 15:16:38 +0530

    Updated index.html

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='blof-index')
+    path('',views.index, name='blog-index'),
     path('home/', views.PostListView.as_view(), name='blog-home'),
     path('user/<str:username>', views.UserPostListView.as_view(template_name='blog/user_post.html'), name='user-posts'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
