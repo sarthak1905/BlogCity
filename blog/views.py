da6c2ffe39cf873b893e1ea299ca9947d6a37039
sarthak1905 <61883822+sarthak1905@users.noreply.github.com>
2020-09-07 10:39:09 +0530

    AOS  updates

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 9
+    paginate_by = 5
 
 class UserPostListView(ListView):
     model = Post
