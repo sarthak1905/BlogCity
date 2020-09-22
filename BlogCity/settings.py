29e3ccdff5ed12f0c70442432d1cef754370320e
sarthak1905 <61883822+sarthak1905@users.noreply.github.com>
2020-09-22 19:02:49 +0530

    Updated server email

--- BlogCity/settings.py
+++ BlogCity/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('BLOG_EMAIL')
-EMAIL_HOST_PASSWORD = os.environ.get('BLOG_PASS')
+EMAIL_HOST_USER = os.environ.get('DEVELOPER_MAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('DEVELOPER_PASS')
